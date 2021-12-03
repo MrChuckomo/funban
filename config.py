976fe45b71d8142b22495f4bc7f869e09d2fdf93
Alexander Kettler <alexander.kettler91@gmail.com>
2021-12-03 18:52:14 +0100

    Readin the config.json using tinydb

--- config.py
+++ config.py
@@ -13,6 +13,6 @@ import os
 # ---------------------------------------------------------------------------------------------------------------------
 
 class Config(object):
-
-    RELEASE_VERSION = '1.0.0'
     SECRET_KEY = os.environ.get('SECRET_KEY') or '123456789abcdfghijklmopqrrstuvwxyz'
+    PROJECT_CTX = os.path.dirname(os.path.abspath(__file__))
+    RES_FOLDER = f'{PROJECT_CTX}{os.sep}index{os.sep}res{os.sep}'
--- index/routes.py
+++ index/routes.py
@@ -11,6 +11,7 @@ from index import app
 from flask import url_for
 from flask import redirect
 from flask import render_template
+from tinydb import TinyDB
 
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -18,8 +19,23 @@ from flask import render_template
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
 def home():
+    db = TinyDB(f'{app.config["RES_FOLDER"]}config.json')
+
+    boards = db.table('boards')
+    profiles = db.table('profiles')
 
     return render_template(
         'home.html',
-        app_title='Funban Home'
+        app_title='Funban Home',
+        active_home='active',
+        all_boards=boards.all(),
+        all_profiles=profiles.all(),
+    )
+
+@app.route('/setup', methods=['GET'])
+def setup():
+    return render_template(
+        'setup.html',
+        app_title='Funban Setup',
+        active_setup='active',
     )
